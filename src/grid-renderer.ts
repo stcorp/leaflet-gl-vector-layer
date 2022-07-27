80ee892681329d4f9a8ce5128fcd133153ddb323
Martti Kaipiainen <martti.kaipiainen@stcorp.nl>
2022-07-27 11:30:25 +0200

    Grid renderer can now handle either midpoints or cell-edges Chooses between the two modes depending on the length of the latitudes/longitudes compared to the length of the given values

--- src/grid-renderer.ts
+++ src/grid-renderer.ts
@@ -5,7 +5,6 @@ import {DataHelper} from "./helpers/data-helper";
 import {IData} from "./types/data";
 import {IPolygon, IQuad, ITriangle} from "./types/polygon";
 
-// Assumes that given latitudes and longitudes are center-points of the polygons.
 export class GridRenderer extends BaseRenderer {
 
     private data: IData;
@@ -21,6 +20,43 @@ export class GridRenderer extends BaseRenderer {
     public processData(callback: () => void) {
         super.processData();
         this.vertices = [];
+        let isMidPoints = this.data.longitudes.length * this.data.latitudes.length === this.data.values.length;
+        if(isMidPoints) {
+            this.processMidPointData(callback);
+        } else {
+            this.processCellEdgeData(callback);
+        }
+    }
+
+    private processCellEdgeData(callback: () => void) {
+
+        for(let latIndex = 0; latIndex < this.data.latitudes.length - 1; latIndex++) {
+            for (let lonIndex = 0; lonIndex < this.data.longitudes.length - 1; lonIndex++) {
+                let index = latIndex*this.data.longitudes.length + lonIndex;
+                let currentValue = this.data.values[index];
+                if(!currentValue && currentValue !== 0) {
+                    continue;
+                }
+                let longitudes = [this.data.longitudes[lonIndex], this.data.longitudes[lonIndex + 1]] as [number, number];
+                let latitudes = [this.data.latitudes[latIndex], this.data.latitudes[latIndex + 1]] as [number, number];
+                let polygon = this.createPolygon(latitudes, longitudes);
+                let meridianChecks = this.calculateMeridianChecks(longitudes);
+                let value = this.normalizeValue(this.data.values[index]);
+                let triangles: ITriangle[] = [];
+                if(meridianChecks.isCrossAntimeridian) {
+                    let newPolygons = this.cutPolygon(polygon);
+                    triangles = this.createTrianglesFromPolygons(newPolygons);
+                } else {
+                    triangles = this.createTrianglesFromQuad(polygon);
+                }
+                this.addTrianglesToVertices(triangles, value)
+            }
+        }
+        this.numPoints = this.vertices.length / 6;
+        callback();
+    }
+
+    private processMidPointData(callback: () => void) {
         let lonCellOffset = 0;
         let latCellOffset = 0;
         if(this.data.longitudes.length >= 2) {
@@ -35,26 +71,17 @@ export class GridRenderer extends BaseRenderer {
                 if(!this.data.values[index] && this.data.values[index] !== 0) {
                     continue;
                 }
-                let longitudes = [this.data.longitudes[lonIndex] - lonCellOffset, this.data.longitudes[lonIndex] + lonCellOffset];
-                let isBelowAntiMeridian = longitudes.some(longitude => (longitude <= -160));
-                let isAboveAntiMeridian = longitudes.some(longitude => (longitude >= 160))
-                let isCrossAntimeridian = isBelowAntiMeridian && isAboveAntiMeridian;
-                let isAbove180 = longitudes.some(longitude => (longitude > 180));
-                let isBelow180 = longitudes.some(longitude => (longitude < 180));
+                let longitudes = [this.data.longitudes[lonIndex] - lonCellOffset, this.data.longitudes[lonIndex] + lonCellOffset] as [number, number];
+                let latitudes = [this.data.latitudes[latIndex] + latCellOffset, this.data.latitudes[latIndex] - latCellOffset] as [number, number];
 
-                let polygon: IQuad = [
-                    [this.data.latitudes[latIndex] + latCellOffset, this.data.longitudes[lonIndex] - lonCellOffset],
-                    [this.data.latitudes[latIndex] + latCellOffset, this.data.longitudes[lonIndex] + lonCellOffset],
-                    [this.data.latitudes[latIndex] - latCellOffset, this.data.longitudes[lonIndex] + lonCellOffset],
-                    [this.data.latitudes[latIndex] - latCellOffset, this.data.longitudes[lonIndex] - lonCellOffset]
-                ];
+                let meridianChecks = this.calculateMeridianChecks(longitudes);
+                let polygon = this.createPolygon(latitudes, longitudes);
                 let value = this.normalizeValue(this.data.values[index]);
-
                 let triangles: ITriangle[] = [];
-                if(isBelow180 && isAbove180) {
+                if(meridianChecks.isBelow180 && meridianChecks.isAbove180) {
                     let newPolygons = this.cutPolygonOver180Longitude(polygon);
                     triangles = this.createTrianglesFromPolygons(newPolygons);
-                } else if(isCrossAntimeridian) {
+                } else if(meridianChecks.isCrossAntimeridian) {
                     let newPolygons = this.cutPolygon(polygon);
                     triangles = this.createTrianglesFromPolygons(newPolygons);
                 } else {
@@ -93,6 +120,30 @@ export class GridRenderer extends BaseRenderer {
             }
         }
         return [newNegativePolygon, newPositivePolygon];
+    }
+
+    private createPolygon(latitudes: [number, number], longitudes: [number, number]): IQuad {
+        return [
+            [latitudes[0], longitudes[0]],
+            [latitudes[0], longitudes[1]],
+            [latitudes[1], longitudes[1]],
+            [latitudes[1], longitudes[0]]
+        ];
+    }
+
+    private calculateMeridianChecks(longitudes: [number, number]) {
+        let isBelowAntiMeridian = longitudes.some(longitude => (longitude <= -160));
+        let isAboveAntiMeridian = longitudes.some(longitude => (longitude >= 160))
+        let isCrossAntimeridian = isBelowAntiMeridian && isAboveAntiMeridian;
+        let isAbove180 = longitudes.some(longitude => (longitude > 180));
+        let isBelow180 = longitudes.some(longitude => (longitude < 180));
+        return {
+            isBelowAntiMeridian,
+            isAboveAntiMeridian,
+            isCrossAntimeridian,
+            isAbove180,
+            isBelow180
+        }
     }
 
 }
